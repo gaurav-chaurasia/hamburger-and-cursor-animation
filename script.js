17976f32bf639c37d2ac4f4bfb954fa27a17c618
Gaurav Chaurasiya <gaurav.181cv155@nitk.edu.in>
2020-06-28 10:56:08 +0530

    add cross animation

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('nav-toggle');
+    document.querySelectorAll('.span').item(1).classList.toggle('slide');
+    document.querySelectorAll('.span').item(0).classList.toggle('cross-down');
+    document.querySelectorAll('.span').item(2).classList.toggle('cross-up');
+    console.log(document.querySelectorAll('.span').item(1).classList);
 });
 
 var div = document.querySelector('.wrapper');
